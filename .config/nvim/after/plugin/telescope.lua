63132fa9b0e5895cfd7bbfbe69cff78d338cfc4d
dima <dimafrunza69@gmail.com>
2024-11-06 10:26:48 +0100

    Better telescope proportions

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,12 +2,18 @@ require('telescope').setup {
   -- You can put your default mappings / updates / etc. in here
   --  All the info you're looking for is in `:help telescope.setup()`
   --
-  -- defaults = {
-  --   mappings = {
-  --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-  --   },
-  -- },
-  -- pickers = { },
+  defaults = {
+    layout_config = {
+      horizontal = {
+        -- width = 0.99,
+        -- height = 0.99,
+        preview_width = 0.65,
+      },
+    },
+  },
+  pickers = {
+    width = 0.3,
+  },
   extensions = {
     ['ui-select'] = {
       require('telescope.themes').get_dropdown(),
