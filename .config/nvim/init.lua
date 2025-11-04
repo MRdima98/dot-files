80d2488322fd912dbbe4e8051cb6a7d607cd88cb
dumitru <dumitru.frunza@widegroup.eu>
2025-11-04 09:39:37 +0100

    configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,7 +14,12 @@ function rsync()
   return ':!rsync -avz --files-from=<(git ls-files) ./ dumitru.frunza@policy-dev.widegroup.eu:~/policyDima<CR>'
 end
 
+function rsync_and_build()
+  return ":!rsync -avz --files-from=<(git ls-files) ./ dumitru.frunza@policy-dev.widegroup.eu:~/policyDima ; ssh dev 'cd policyDima/ ; source /usr/local/nvm/nvm.sh ; yarn build' <CR>"
+end
+
 vim.keymap.set('n', '<leader>t', rsync(), { noremap = true, silent = true, desc = 'Run ls in shell' })
+vim.keymap.set('n', '<leader>r', rsync_and_build(), { noremap = true, silent = true, desc = 'Run ls in shell' })
 
 -- in your nvim config
 vim.filetype.add({
